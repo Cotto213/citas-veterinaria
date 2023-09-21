616fc0b61b77e85b5400a770cc21497c8dd8854b
Cotto213 <alfonsocotto5b12@gmail.com>
2023-09-20 18:35:22 -0600

    App.js modified

--- citas-veterinaria/App.js
+++ citas-veterinaria/App.js
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableHighlight, TouchableWithoutFeedback, Keyboard, Platform } from 'react-native';
+import Cita from './src/components/Cita';
+import Formulario from './src/components/Formulario';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Colors from './src/utils/colors'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App =()=>{
+
+  const [citas, setCitas]=useState([]);
+  const [mostrarForm, guardarMostrarForm]=useState(false);
+
+  useEffect(()=>{
+    const obtenerCitasStorage=async()=>{
+      try{
+        const citasStorage = await AsyncStorage.getItem('citas');
+        if(citasStorage){
+          setCitas(JSON.parse(citasStorage));
+        }
+        }catch(error){
+          console.log(error);
+        }
+    }
+    obtenerCitasStorage();
+  },[]);
+
+  const eliminarPaciente=id=>{
+    const citasFiltradas = citas.filter(cita=>cita.id !== id);
+    setCitas(citasFiltradas);
+    guardarCitasStorage(JSON.stringify(citasFiltradas));
+  }
+
+  const mostrarFormulario =()=>{
+    guardarMostrarForm(!mostrarForm);
+  }
+
+  const cerrarTeclado=()=>{
+    Keyboard.dismiss();
+  }
+const guardarCitasStorage=async(citasJSON)=>{
+  try{
+    await AsyncStorage.setItem('citas', citasJSON);   
+   }catch(error){
+    console.log(error);
+   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+return (
+
+<TouchableWithoutFeedback onPress={()=>cerrarTeclado()}>
+  <View style={styles.contenedor}>
+    <Text style={styles.titulo}>Administrador de Citas</Text>
+  <View>
+  <TouchableHighlight onPress={()=>mostrarFormulario() }
+  style={styles.btnMostrarForm}>
+    <Text style={styles.textoMostrarForm}>
+      {mostrarForm ? 'Cancelar Crear Cita': 'Crear Nueva Cita'}
+    </Text>
+  </TouchableHighlight>
+  </View>
+  </View>
+</TouchableWithoutFeedback>
+
+
+
+)
+
+
+
+}
